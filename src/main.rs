6dab0f11addf3497cd52ba1416780467c710fce6
dali <dalibraham.18m@gmail.com>
2026-07-15 17:20:34 +0100

    initial setup and env vars

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+use dotenvy;
+
+
+
+mod state;
+use sqlx::MySqlPool;
+use state::AppState;
+
+mod config;
+use config::Config;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    dotenvy::dotenv().ok();   //load env variables
+    let my_config = Config{
+        database_url: std::env::var("database_url").expect("database_url should be set"),
+        jwt_secret : std::env::var("jwt_secret").expect("jwt_secret should be set"),
+        port : std::env::var("port").expect("port should be set"),
+    };
+    
+    let my_app_state = AppState{
+        pool : MySqlPool::connect(&my_config.database_url).await.expect("couldn't connect to MySql"),
+    };
+    HttpServer::new(move ||{
+        App::new()
+            .app_data(my_app_state.clone())
+    }).bind(("127.0.0.1",my_config.port.parse::<u16>().unwrap()))?
+    .run()
+    .await
 }
