9665adc4a09ceb67c1e048df7ce719ee949d6f3d
dali <dalibraham.18m@gmail.com>
2026-07-22 00:21:28 +0100

    implemented project handlers

--- src/routes/project_route.rs
+++ src/routes/project_route.rs
@@ -3,6 +3,14 @@ use actix_web::{
     web::{ServiceConfig, scope},
 };
 
+use crate::handlers::project_handlers::{createProject, deleteProject, getAllUserProjects, getProject, updateProject};
+
 pub fn config(cfg: &mut ServiceConfig) {
-    cfg.service(scope("/project"));
+    cfg.service(scope("/project"))
+        .service(createProject)
+        .service(getProject)
+        .service(getAllUserProjects)
+        .service(updateProject)
+        .service(deleteProject);
+    
 }
